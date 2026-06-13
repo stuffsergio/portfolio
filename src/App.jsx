4ccb6d098f94361a852e6f8f156f842a1ecb8f1f
stuffsergio <sergioperezmontalvo@gmail.com>
2026-06-13 23:20:53 +0200

    v1

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      <div>
+        <h2>Sergio Pérez</h2>
+        <p>FullStack Web Developer</p>
+      </div>
+      <div>
+        <p>
+          I'm passionate about building the backend and frontend, to understand
+          how the product must be delivered to users
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
